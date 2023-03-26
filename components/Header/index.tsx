13843a5fa23ce8c10e293fead0d10a75117d0fbd
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2023-03-25 23:53:51 -0700

    create Header component

---
+++ components/Header/index.tsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header className='flex w-full bg-gray-800 text-primary-500 h-[10vh]'>
+      <span className='text-4xl h-full flex items-center'>Ryde</span>
+    </header>
+  );
+};
+export default Header;
