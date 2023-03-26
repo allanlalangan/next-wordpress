d21f33a1682ec74d4723899e0f463f7da08f07af
allanlalangan <73400708+allanlalangan@users.noreply.github.com>
2023-03-25 23:01:52 -0700

    update head and page layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,22 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Ride Share</title>
+        <title>Ride Booking</title>
         <meta name='description' content='Ride share booking website' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Component {...pageProps} />
+      <main className=''>
+        <Component {...pageProps} />
+      </main>
     </>
   );
 }
